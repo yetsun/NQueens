97e6d807f6a66edd3d53b93c0798795c8267d5f8
jye <jye@coupang.com>
2017-01-07 10:04:02 +0900

    add comments

--- scala/MyNQueens.scala
+++ scala/MyNQueens.scala
@@ -1,5 +1,23 @@
+/*
+
+The idea is to place the queens row by row. (start from row 0. each row has one queen)
+When placing a queen on row k, we assume that the solutions for row from row 0 to row k-1 are ready. 
+(Invariant k-1 solution exists)
+Then the only thing left is to check each column on row k is OK for the existing solutions of row 0 to row k-1.
+
+*/
+
+
 object MyNQueens extends App{
 
+
+  /*
+   * Return value is a set of list. 
+   * Each list is a solution. 
+   * The list index is the row number is reverse order. The last row is the first element in the list 
+   * (This is due to the the nature scala list, i.e. it's easier to append to the head)
+   * The list value is the column index.
+   */
   def queens(size: Int): Set[List[Int]] = {
 
     def q(row: Int): Set[List[Int]] = {
@@ -15,11 +33,11 @@ object MyNQueens extends App{
     }
 
 
+    /*
+     * check if it is not the same col, not diagonal
+     * not the same row, implied
+     */
     def isSafe(col: Int, solution: List[Int]): Boolean = {
-
-      //check if it is not the same col, not diagonal
-      //not the same row, implied
-
       val row = solution.length
       val queenRowCols = row-1 to 0 by -1 zip solution
 
